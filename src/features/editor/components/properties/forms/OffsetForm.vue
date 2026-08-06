fd3f3e9a09cc1771c30fe7e264a14481fdb2c32e
@rbgirard <rbgirard@users.noreply.github.com>
2026-08-06 09:20:59 -0700

    Revert "Move Angle Sliders"
    This reverts commit e2f3f928b964dd2c66d18c026cdd0448ca56d8a8.

--- src/features/editor/components/properties/forms/OffsetForm.vue
+++ src/features/editor/components/properties/forms/OffsetForm.vue
@@ -66,9 +66,8 @@ const data0 = computed<DynamicVal>(() => ({
   name: '0',
   neg: true,
   label: pMOVE.value === 'xyz' ? 'Horizontal' : 'Plane',
-  max: pMOVE.value === 'xyz' ? 10 : 180,
-  min: pMOVE.value === 'xyz' ? -10 : -180,
-  step: pMOVE.value === 'xyz' ? 1 : 45,
+  max: pMOVE.value === 'xyz' ? 10 : 45,
+  min: pMOVE.value === 'xyz' ? -10 : -45,
   float: float.value,
   mult: pMOVE.value === 'xyz' ? mult.value * -1 : mult.value,
 }))
@@ -77,9 +76,8 @@ const data1 = computed<DynamicVal>(() => ({
   name: '1',
   neg: pMOVE.value === 'xyz',
   label: pMOVE.value === 'xyz' ? 'Vertical' : 'Arc',
-  max: pMOVE.value === 'xyz' ? 10 : 180,
-  min: pMOVE.value === 'xyz' ? -10 : -180,
-  step: pMOVE.value === 'xyz' ? 1 : 45,
+  max: pMOVE.value === 'xyz' ? 10 : 45,
+  min: pMOVE.value === 'xyz' ? -10 : -45,
   float: float.value,
   mult: mult.value,
 }))
